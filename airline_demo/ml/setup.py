20be231342371b1608749c60cb2d60da5fa047af
Moo-YewTsing <mooyewtsing@gmail.com>
2019-10-27 19:47:05 -0400

    --amend

--- airline_demo/ml/setup.py
+++ airline_demo/ml/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ['tensorflow-transform==0.14.0']
+REQUIRED_PACKAGES = ['tensorflow-transform==0.14.0', 'tensorflow-model-analysis==0.14.0']
 
 setup(
     name='flight',
